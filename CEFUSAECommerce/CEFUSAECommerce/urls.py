8fd2614ec855839e54113f4edb6f2d094fb30373
AndresVelez31 <afveleza@eafit.edu.co>
2026-05-29 04:38:54 -0500

    fix: resolver colision de ruta /admin entre frontend y django

--- CEFUSAECommerce/CEFUSAECommerce/urls.py
+++ CEFUSAECommerce/CEFUSAECommerce/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/',          admin.site.urls),
+    path('django-admin/',   admin.site.urls),
     path('api/orders/',     include('orders.urls')),
     path('api/customers/',  include('customers.urls')),
     path('api/admin/',      include('CEFUSAECommerce.admin_urls')),
